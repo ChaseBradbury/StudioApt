065ddc82c1bffeff15f41ae17cc8412da75c09f6
Matt <mattscorca@gmail.com>
2016-12-10 16:43:20 -0800

    Separated out furniture js

--- canvas.js
+++ canvas.js
@@ -114,58 +114,7 @@ function keyPress(event) {
 	}
 }
 
-var couch = new roomObject(grid.length * cellSize + 10, 10, 2, 1, "#0000ff", "#ccccff");
-
-function roomObject(x, y, length, height, color, highlightColor) {
-	this.x = x;
-	this.y = y;
-	this.initX = x;
-	this.initY = y;
-	this.length = length;
-	this.height = height;
-	this.sizeX = cellSize*length;
-	this.sizeY = cellSize*height;
-	this.color = color;
-	this.highlightColor = highlightColor;
-	this.clicked = false;
-
-	this.isClicked = function(x, y) {
-		if (x > this.x && x < this.x + this.sizeX && y > this.y && y < this.y + this.sizeY) {
-			this.clicked = !this.clicked;
-			return true;
-		}
-	}
-
-	this.rotate = function() {
-		var tmpl = this.length;
-		var tmpx = this.sizeX;
-		this.length = this.height;
-		this.sizeX = this.sizeY;
-		this.height = tmpl;
-		this.sizeY = tmpx;
-	}
-
-	this.isSafe = function() {
-		var gx = Math.floor(this.x/cellSize);
-		var gy = Math.floor(this.y/cellSize);
-		for (var i = gx; i < this.length + gx; ++i) {
-			for (var j = gy; j < this.height + gy; ++j) {
-				if (grid[i] == null || grid[i][j] == null || !grid[i][j].usable) return false; 
-			}
-		}
-		return true;
-	}
-
-	this.draw = function() {
-		ctx.strokeStyle="#000000";
-		ctx.strokeRect(this.x, this.y, this.sizeX, this.sizeY);
-		
-		if(!this.isSafe()) ctx.fillStyle=dangerColor;
-		else if(this.clicked) ctx.fillStyle=this.highlightColor;
-		else ctx.fillStyle=this.color;
-		ctx.fillRect(this.x, this.y, this.sizeX, this.sizeY);
-	}
-}
+var couch = new furniture(grid.length * cellSize + 10, 10, 2, 1, "#0000ff", "#ccccff");
 
 setInterval(draw, 30);
 
